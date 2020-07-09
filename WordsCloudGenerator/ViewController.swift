04c253c4dc06fc5e36a2cecd23a526dcfd682e1e
jose <jose@tootoot.co.uk>
2020-07-09 09:22:01 +0100

    Initial Commit

---
+++ WordsCloudGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordsCloudGenerator
+//
+//  Created by Jose Barba on 09/07/2020.
+//  Copyright © 2020 Jose Barba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
