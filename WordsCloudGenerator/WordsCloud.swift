2917d00d32054383e3f248765adac21fae51f42f
jose <jose@tootoot.co.uk>
2020-07-09 14:43:52 +0100

    first commit

---
+++ WordsCloudGenerator/WordsCloud.swift
@@ -0,0 +1,21 @@
+//
+//  WordsCloud.swift
+//  WordsCloudGenerator
+//
+//  Created by Jose Barba on 09/07/2020.
+//  Copyright © 2020 Jose Barba. All rights reserved.
+//
+
+import UIKit
+
+class WordsCloud: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
